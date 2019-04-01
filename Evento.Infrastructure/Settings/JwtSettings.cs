2f586a0f5ad1e713491aae1c431c5bc87394948f
Rafał Nowociński <vito24222@gmail.com>
2019-04-01 09:02:18 +0200

    adding a class to store settings

---
+++ Evento.Infrastructure/Settings/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Evento.Infrastructure.Settings
+{
+    public class JwtSettings
+    {
+        public int ExpiryInMinutes { get; set; }
+        public string SigningKey { get; set; }
+        public string Site { get; set; }
+    }
+}
