515e50cf94f6faf2c8bc10e11bd48ebea3d0ce6e
Rafał Nowociński <vito24222@gmail.com>
2019-05-06 17:49:12 +0200

    add xUnit tests

---
+++ Tests/AccountRepositiryTests.cs
@@ -0,0 +1,32 @@
+﻿using Evento.Core.Domain;
+using Evento.Core.Repositories;
+using Evento.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class AccountRepositiryTests
+    {
+        [Fact]
+        public async Task Poprawne_dodanie_nowego_użytkownika_do_bazy_danych()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                      .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                      .Options;
+            var context = new DataBaseContext(options);
+
+            Account account = new Account(Guid.NewGuid(), "test", "test", "test@test.com", "secret");
+            IAccountRepository accountRepository = new AccountRepository(context);
+            // Act
+            await accountRepository.AddAsync(account);
+            Account existAccount = await accountRepository.GetAsync(account.Id);
+            // Assert
+            Assert.Equal(account, existAccount);
+        }
+    }
+}
---
+++ Tests/UserServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Evento.Core.Domain;
+using Evento.Core.Repositories;
+using Evento.Infrastructure.Services.User;
+using Evento.Infrastructure.Services.User.JwtToken;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async Task Testowanie_prawidłowej_rejestracji_użytkownika()
+        {
+            // Arrange
+            var accountRepositoryMock = new Mock<IAccountRepository>();
+            var jwtHandlerMock = new Mock<IJwtHandler>();
+            var mapperMock = new Mock<IMapper>();
+            var userService = new UserService(accountRepositoryMock.Object,
+                jwtHandlerMock.Object, mapperMock.Object);
+            // Act
+            await userService.RegisterAsync(Guid.NewGuid(), "test@test.com", "test", "secret", "test");
+            // Assert
+            accountRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Account>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Testowanie_prawidłowej_pobierania_użytkownika()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                      .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                      .Options;
+            var context = new DataBaseContext(options);
+            Account account = new Account(Guid.NewGuid(), "test", "test", "test@test.com", "secret");
+            var accountRepositoryMock = new Mock<IAccountRepository>();
+            var jwtHandlerMock = new Mock<IJwtHandler>();
+            var mapperMock = new Mock<IMapper>();
+            var userService = new UserService(accountRepositoryMock.Object,
+                jwtHandlerMock.Object, mapperMock.Object);
+            // Act
+            accountRepositoryMock.Setup(x => x.GetAsync(account.Id)).ReturnsAsync(account);
+            var accountDTO = await userService.GetAccountAsync(account.Id);
+            // Assert
+            accountRepositoryMock.Verify(x => x.GetAsync(account.Id), Times.Once);
+        }
+    }
+}
